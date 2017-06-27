c8675f6880c6665ab6fa0c83458334a2fc2c6405
lunax <albertofly10@gmail.com>
2017-06-27 23:17:42 +0200

    Add TabLayout

--- app/src/main/java/com/example/android/ud839_miwok/MainActivity.java
+++ app/src/main/java/com/example/android/ud839_miwok/MainActivity.java
@@ -2,6 +2,8 @@ package com.example.android.ud839_miwok;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -13,6 +15,22 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        // Find the view pager that will allow the user to swipe between fragments
+        ViewPager viewPager = (ViewPager) findViewById(R.id.viewpager);
+
+        // Create an adapter that knows which fragment should be shown on each page
+        CategoryAdapter adapter = new CategoryAdapter(this, getSupportFragmentManager());
+
+        // Set the adapter onto the view pager
+        viewPager.setAdapter(adapter);
+
+        TabLayout tabLayout = (TabLayout) findViewById(R.id.sliding_tabs);
+        tabLayout.setupWithViewPager(viewPager);
+
+
+        /*
+
 
         TextView numbers = (TextView) findViewById(R.id.numbers);
         numbers.setOnClickListener(new OnClickListener() {
@@ -69,6 +87,7 @@ public class MainActivity extends AppCompatActivity {
                 startActivity(phrasesIntent);
             }
         });
+         */
     }
 
 
